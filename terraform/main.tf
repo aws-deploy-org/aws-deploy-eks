082c94aec0582a21f244dc44428e4592f42bd15a
Sonam Ekbote <soekbote@deloitte.com>
2026-03-06 00:07:53 +0530

    Trigger Pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,6 @@ module "vpc" {
     azs        = ["us-east-1a", "us-east-1b", "us-east-1c"]
     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    tags = {environment = "dev"}
+    tags = {env = "dev"}
   }
 }
