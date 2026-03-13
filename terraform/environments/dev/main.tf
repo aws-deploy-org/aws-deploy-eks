8b0f8ad5213df269cb91d60f6b5f3e33e4d83b8a
Sonam Ekbote <soekbote@deloitte.com>
2026-03-13 15:49:41 +0530

    Trigger Workflow

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,10 +1,7 @@
 module "vpc" {
   source = "../../modules/vpc"
   vpc = {
-    cidr_block = "10.0.0.0/16" #vpc cidr block
-    #  azs        = ["us-east-1a", "us-east-1b", "us-east-1c"]
-    #   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    #  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    cidr_block = "10.0.0.0/16"
     tags = { env = "dev" }
   }
 
